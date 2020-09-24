8fc89d7f6fec81f09e0352eac8707b325204af7d
marcsteenbergh <70932667+marcsteenbergh@users.noreply.github.com>
2020-09-24 14:32:07 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-	
+    Player = randint(1, 4)
+    basic.showNumber(Player)
+    basic.pause(500)
+    basic.clearScreen()
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    if (Player == 1) {
+        basic.showNumber(Player + 1)
+    } else if (Player == 2) {
+        basic.showNumber(Player + 2)
+    } else if (Player == 3) {
+        basic.showNumber(Player + 3)
+    } else if (Player == 1) {
+    	
+    } else {
+    	
+    }
 })
+let Player = 0
+Player = 0
